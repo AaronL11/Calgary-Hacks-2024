20b4d730ade47f9f5d5fea581d8e6caaab4a88ca
Aaron Lengyel <aaron.lengyel@ucalgary.ca>
2024-02-17 16:35:43 -0700

    models and files added

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+mod api;
+mod models;
+mod repository;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::{get, http::Status, serde::json::Json};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from rust and mongoDB")))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![hello])
 }
