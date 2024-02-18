fd2c88dc13a0ceab6c0410816f30a1491f759b76
walija101 <walija101@gmail.com>
2024-02-17 17:43:58 -0700

    moving pngs

--- discoveryyc/src/App.js
+++ discoveryyc/src/App.js
@@ -3,27 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import TopBar from './Components/TopBar';
+import HomePage from './pages/homePage';
 
 function App() {
   return (
     <ChakraProvider>
       <TopBar/>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <HomePage/>
     </ChakraProvider>
   )
 }
---
+++ discoveryyc/src/pages/homePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <main>
+        <img src={require("../../public/calgaryskyline.png")} alt="City Skyline" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
